a26502d4560ddaed6c1222901b87fc81b6d67aa3
AlonaAkimova <aliona.akimova@gmail.com>
2023-05-29 11:17:09 +0700

    changes

--- starter/script.js
+++ starter/script.js
@@ -6,6 +6,7 @@
 // document.querySelector('.score').textContent = 10;
 // document.querySelector('.guess').value = 23;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -14,5 +15,21 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number 🥺';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number 👍';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high! 😖';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 🥺';
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low! 😖';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game 🥺';
+    }
   }
 });
