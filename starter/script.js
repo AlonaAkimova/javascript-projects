16858abe8cda58d66dc76c68e87e7da8ee5c6fac
AlonaAkimova <aliona.akimova@gmail.com>
2023-05-29 10:56:59 +0700

    changes

--- starter/script.js
+++ starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number 👍';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// document.querySelector('.guess').value = 23;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number 🥺';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number 👍';
+  }
+});
